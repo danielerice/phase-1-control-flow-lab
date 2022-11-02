e5d004414db3ce98d3f5947e2ff5f317afae478e
Daniel Rice <danielrice5627@gmail.com>
2022-11-02 09:39:24 -0700

    commit tree c164944913e017f6f700555c733355fd335ea52c

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  
+  if (someNumber <= 400){
+      return `This one is on me!`;
+    
+    } else if (someNumber > 400 && someNumber <= 2000){
+      return `That will be twenty bucks.`;
+    
+    } else if (someNumber > 2500) {
+      return `No can do.`;
+    
+    } else if (someNumber > 2000){
+      return `I will gladly take your thirty bucks.`;
+    }
+
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+   return someCity === `NYC` ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(theTip){
+    switch (theTip){
+      case "generous":
+        return "Thank you so much.";
+        break;
+      case 'not as generous':
+        return "Thank you.";
+        break;
+      case 'thanks for everything':
+        return "Bye."
+        break;
+      }
 }
